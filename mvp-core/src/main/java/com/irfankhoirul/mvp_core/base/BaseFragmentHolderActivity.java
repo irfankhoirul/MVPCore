01e10822e3cfbe5a09a5b01bbf6f1fe84f379b11
irfankhoirul <irfankhoirul@gmail.com>
2017-05-30 12:40:57 +0700

    First Release

---
+++ mvp-core/src/main/java/com/irfankhoirul/mvp_core/base/BaseFragmentHolderActivity.java
@@ -0,0 +1,50 @@
+package com.irfankhoirul.mvp_core.base;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.irfankhoirul.mvp_core.R;
+import com.irfankhoirul.mvp_core.R2;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public abstract class BaseFragmentHolderActivity extends BaseActivity {
+
+    @BindView(R2.id.flFragmentContainer)
+    protected FrameLayout flFragmentContainer;
+    @BindView(R2.id.btOptionMenu)
+    protected ImageButton btOptionMenu;
+    @BindView(R2.id.tvToolbarTitle)
+    protected TextView tvToolbarTitle;
+    @BindView(R2.id.ivIcon)
+    protected ImageView ivIcon;
+    @BindView(R2.id.btBack)
+    protected ImageButton btBack;
+    @BindView(R2.id.vMenuBarShadow)
+    protected View vMenuBarShadow;
+    @BindView(R2.id.rlActivityFragmentHolder)
+    protected RelativeLayout rlActivityFragmentHolder;
+
+    @Override
+    protected void initializeView() {
+        setContentView(R.layout.activity_fragment_holder);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected TextView getTvToolbarTitle() {
+        return tvToolbarTitle;
+    }
+
+    @OnClick(R2.id.btBack)
+    public void btBack() {
+        onBackPressed();
+    }
+
+}
---
+++ mvp-core/src/main/java/com/irfankhoirul/mvp_core/utils/Constant.java
@@ -0,0 +1,15 @@
+package com.irfankhoirul.mvp_core.utils;
+
+/**
+ * Berisi konstanta yang digunakan pada lebih dari 1 kelas
+ *
+ * @author Irfan Khoirul Muhlishin - irfankhoirul@gmail.com
+ * @since 1.0
+ */
+
+public class Constant {
+    public static final int STATUS_INFO = 0;    // Blue
+    public static final int STATUS_SUCCESS = 1; // Green
+    public static final int STATUS_WARNING = 2; // Orange
+    public static final int STATUS_ERROR = 3;   // Red
+}
