905a0f2272e195eff44f3ee6a96420a12b134729
irfankhoirul <irfankhoirul@gmail.com>
2017-07-04 12:30:34 +0700

    Some tweaks

--- mvp-core/src/main/java/com/irfankhoirul/mvp_core/base/BasePojo.java
+++ mvp-core/src/main/java/com/irfankhoirul/mvp_core/base/BasePojo.java
@@ -1,31 +1,46 @@
 package com.irfankhoirul.mvp_core.base;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-
-import org.parceler.Parcel;
 
 /**
  * Merupakan Super Class dari kelas model dalam aplikasi. Berisi atribut standar yaitu id,
  * createdAt, dan updatedAt
  *
- * @author  Irfan Khoirul Muhlishin - irfankhoirul@gmail.com
+ * @author Irfan Khoirul Muhlishin - irfankhoirul@gmail.com
  * @since 1.0 (7 November 2016)
  */
 
-@Parcel
-public class BasePojo {
+public class BasePojo implements Parcelable {
+    public static final Creator<BasePojo> CREATOR = new Creator<BasePojo>() {
+        @Override
+        public BasePojo createFromParcel(Parcel in) {
+            return new BasePojo(in);
+        }
+
+        @Override
+        public BasePojo[] newArray(int size) {
+            return new BasePojo[size];
+        }
+    };
     @SerializedName("id")
     @Expose
     protected int id;
-
     @SerializedName("created_at")
     @Expose
     protected String createdAt;
-
     @SerializedName("updated_at")
     @Expose
     protected String updatedAt;
+
+    protected BasePojo(Parcel in) {
+        id = in.readInt();
+        createdAt = in.readString();
+        updatedAt = in.readString();
+    }
 
     public int getId() {
         return id;
@@ -49,5 +64,17 @@ public class BasePojo {
 
     public void setUpdatedAt(String updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(createdAt);
+        dest.writeString(updatedAt);
     }
 }
